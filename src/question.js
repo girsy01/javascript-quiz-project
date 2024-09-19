b7a2290a6be67a69a2e508f8efcbead09c72f5a0
Franziska Scheer <franzi-scheer@web.de>
2024-09-19 21:35:32 +0200

    current status after day1 of coding

--- src/question.js
+++ src/question.js
@@ -1,7 +1,42 @@
 class Question {
-    // YOUR CODE HERE:
-    //
-    // 1. constructor (text, choices, answer, difficulty)
+  constructor(text, choices, answer, difficulty) {
+    this.text = text;
+    this.choices = choices;
+    this.answer = answer;
+    this.difficulty = difficulty;
+  }
 
-    // 2. shuffleChoices()
+  shuffleChoices() {
+    
+    let currentIndex = this.choices.length;
+
+    // While there remain elements to shuffle...
+    while (currentIndex != 0) {
+      // Pick a remaining element...
+      let randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex--;
+
+      // And swap it with the current element.
+      [this.choices[currentIndex], this.choices[randomIndex]] = [
+        this.choices[randomIndex],
+        this.choices[currentIndex],
+      ];
+    }
+  }
 }
+
+
+// const testChoices = [
+//   "choice1",
+//   "choice2",
+//   "choice3",
+//   "choice4",
+//   "choice5",
+//   "choice6",
+//   "choice7",
+// ];
+
+// const testQuestion = new Question("testText", testChoices, "answer", 1);
+// console.log("Before shuffling:", testQuestion.choices);
+// testQuestion.shuffleChoices();
+// console.log("After shuffling:", testQuestion.choices);
--- src/quiz.js
+++ src/quiz.js
@@ -1,6 +1,15 @@
 class Quiz {
     // YOUR CODE HERE:
-    //
+    constructor(questions, timeLimit, timeRemaining) {
+        this.questions = questions;
+        this.timeLimit = timeLimit;
+        this.timeRemaining = timeRemaining;
+        this.correctAnswers = 0;
+        this.currentQuestionIndex = 0;
+    }
+
+    getQuestion()
+
     // 1. constructor (questions, timeLimit, timeRemaining)
 
     // 2. getQuestion()
