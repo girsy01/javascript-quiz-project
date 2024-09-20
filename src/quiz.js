662b64341ec9f25e6359f1400f601d4be4d47786
Erian Flores <erian.flores@gmail.com>
2024-09-20 20:10:32 +0200

    Second Revision

--- src/quiz.js
+++ src/quiz.js
@@ -8,7 +8,38 @@ class Quiz {
         this.currentQuestionIndex = 0;
     }
 
-    getQuestion()
+    getQuestion() {
+        return this.questions [this.currentQuestionIndex];
+    }
+
+    moveToNextQuestion() {
+        return this.currentQuestionIndex =+ 1;
+    }
+
+
+    shuffleQuestions() {
+        let currentIndex1 = this.questions.length;
+
+    while (currentIndex1 != 0) {
+      let randomIndex1 = Math.floor(Math.random() * currentIndex1);
+      currentIndex1--;
+
+      [this.questions[currentIndex1], this.questions[randomIndex1]] = [
+        this.questions[randomIndex1],
+        this.questions[currentIndex1],
+      ];
+    }
+    }
+
+    checkAnswer(answer) {
+        const currentQuestion = this.question[this.currentQuestionIndex];
+
+        if (answer === currentQuestion.correctAnswers) {
+            this.correctAnswers++;
+        }
+    }
+
+
 
     // 1. constructor (questions, timeLimit, timeRemaining)
 
