0f670f9dd65ab32b16e89d515482e0dcf494a06c
Franziska Scheer <franzi-scheer@web.de>
2024-09-20 20:46:08 +0200

    added last function of day1

--- src/quiz.js
+++ src/quiz.js
@@ -1,24 +1,22 @@
 class Quiz {
-    // YOUR CODE HERE:
-    constructor(questions, timeLimit, timeRemaining) {
-        this.questions = questions;
-        this.timeLimit = timeLimit;
-        this.timeRemaining = timeRemaining;
-        this.correctAnswers = 0;
-        this.currentQuestionIndex = 0;
-    }
+  constructor(questions, timeLimit, timeRemaining) {
+    this.questions = questions;
+    this.timeLimit = timeLimit;
+    this.timeRemaining = timeRemaining;
+    this.correctAnswers = 0;
+    this.currentQuestionIndex = 0;
+  }
 
-    getQuestion() {
-        return this.questions [this.currentQuestionIndex];
-    }
+  getQuestion() {
+    return this.questions[this.currentQuestionIndex];
+  }
 
-    moveToNextQuestion() {
-        return this.currentQuestionIndex =+ 1;
-    }
+  moveToNextQuestion() {
+    this.currentQuestionIndex += 1; //no return needed
+  }
 
-
-    shuffleQuestions() {
-        let currentIndex1 = this.questions.length;
+  shuffleQuestions() {
+    let currentIndex1 = this.questions.length;
 
     while (currentIndex1 != 0) {
       let randomIndex1 = Math.floor(Math.random() * currentIndex1);
@@ -29,27 +27,20 @@ class Quiz {
         this.questions[currentIndex1],
       ];
     }
+  }
+
+  checkAnswer(answer) {
+    const currentQuestion = this.questions[this.currentQuestionIndex]; //@Erian: you just had a typo ('questions' instead of 'question')
+
+    if (answer === currentQuestion.answer) {
+      this.correctAnswers++;
     }
+  }
 
-    checkAnswer(answer) {
-        const currentQuestion = this.question[this.currentQuestionIndex];
-
-        if (answer === currentQuestion.correctAnswers) {
-            this.correctAnswers++;
-        }
+  hasEnded() {
+    if (this.currentQuestionIndex === this.questions.length) {
+      return true;
     }
-
-
-
-    // 1. constructor (questions, timeLimit, timeRemaining)
-
-    // 2. getQuestion()
-    
-    // 3. moveToNextQuestion()
-
-    // 4. shuffleQuestions()
-
-    // 5. checkAnswer(answer)
-
-    // 6. hasEnded()
+    return false;
+  }
 }
